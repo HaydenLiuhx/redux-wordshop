55c701741de9f63c6270ab03edca429fcb1cac86
HaydenLiu <huangxunliu@gmail.com>
2020-08-31 17:34:41 +1000

    redux_custom

---
+++ redux_wordplace/src_redux_custom/index.js
@@ -0,0 +1,18 @@
+/*
+    Entrance
+    ReactDOM.render(）的作用是将<App/>的内容渲染到根“root”中去。
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store from './redux/store'
+
+console.log('state', store.getState())
+//将App组件标签渲染到index页面的div上
+ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+
+//给store绑定状态更新的监听
+store.subscribe(() => { //store内部的状态数据发生改变时回调
+    //重新渲染App组件
+    ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+})
---
+++ redux_wordplace/src_redux_custom/redux/reducer.js
@@ -0,0 +1,39 @@
+/* 
+    reducer function modul
+    manage state data
+*/
+import { INCREMENT, DECREMENT } from './action-types'
+import { combineReducers } from '../lib/redux'
+/* 
+    管理count状态数据的reducer
+*/
+function count(state = 1, action) {
+    console.log('count()', state, action)
+    switch (action.type) {
+        case INCREMENT:
+            return state + action.data
+        case DECREMENT:
+            return state - action.data
+        default:
+            return state
+    }
+}
+
+/* 
+    管理用户信息
+*/
+function user(state = {}, action) {
+    console.log('user()', state, action)
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+/* 
+    返回一个整合后的reducer
+    管理总的状态: {count: 1, user: {}}
+*/
+export default combineReducers({
+    count,
+    user
+})
